9cbb34512be93cca74f571c0a07a228a8709eb0a
Masahiko AMANO <hiko-19@ya.ru>
2022-12-25 01:18:10 +0300

    fix(lib): minor fixes

--- src/tanabata/sasa.c
+++ src/tanabata/sasa.c
@@ -20,6 +20,16 @@ int tanabata_sasa_add(Tanabata *tanabata, const char *path) {
 }
 
 int tanabata_sasa_rem_by_id(Tanabata *tanabata, uint64_t sasa_id) {
+    if (sasa_id == HOLE_ID || sasa_id >= tanabata->sasahyou.size) {
+        return 1;
+    }
+    Kazari *current_kazari = tanabata->shoppyou.database;
+    for (uint64_t j = 0; j < tanabata->shoppyou.size; j++) {
+        if (current_kazari->sasa_id == sasa_id) {
+            current_kazari->sasa_id = HOLE_ID;
+        }
+        current_kazari++;
+    }
     return sasa_rem(&tanabata->sasahyou, sasa_id);
 }
 
@@ -27,6 +37,13 @@ int tanabata_sasa_rem_by_path(Tanabata *tanabata, const char *path) {
     Sasa *current_sasa = tanabata->sasahyou.database;
     for (uint64_t i = 0; i < tanabata->sasahyou.size; i++) {
         if (current_sasa->id != HOLE_ID && strcmp(current_sasa->path, path) == 0) {
+            Kazari *current_kazari = tanabata->shoppyou.database;
+            for (uint64_t j = 0; j < tanabata->shoppyou.size; j++) {
+                if (current_kazari->sasa_id == current_sasa->id) {
+                    current_kazari->sasa_id = HOLE_ID;
+                }
+                current_kazari++;
+            }
             return sasa_rem(&tanabata->sasahyou, current_sasa->id);
         }
         current_sasa++;
