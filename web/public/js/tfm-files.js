b618ecbd52239c58d5f05b9de3a90ba3e9c4465a
Masahiko AMANO <hiko-19@ya.ru>
2023-01-29 18:20:39 +0300

    refactor(web): separate TFM files page

---
+++ web/public/js/tfm-files.js
@@ -0,0 +1,77 @@
+$(window).on("load", function () {
+	sasa_load();
+	sasahyou.forEach((sasa) => {
+		$(".contents-wrapper").append(`<div class="item sasa" id="s${sasa.id}" title="${sasa.path.split('/').slice(-1)}" style="background-image: url(${"/thumbs/" + sasa.path})"><div class="overlay"></div></div>`);
+	});
+	tanzaku_load();
+	sappyou.forEach((tanzaku) => {
+		$(".list").append(`<div class="list-item tanzaku" id="t${tanzaku.id}">${tanzaku.name}</div>`);
+	});
+	kazari_load();
+});
+
+$(document).on("dblclick", ".item", function (e) {
+	let id = parseInt($(this).attr("id").slice(1));
+	sasahyou.every(sasa => {
+		if (sasa.id === id) {
+			current_sasa = sasa;
+			return false;
+		}
+		return true;
+	});
+	$(".item.selected").removeClass("selected");
+	$(".menu-wrapper").css("display", "flex");
+	$("#name").val(decodeURI(current_sasa.path));
+	$("#btn-full").attr("href", "/files/" + current_sasa.path);
+	let resp = tdb_query("$TFM", 24, '' + id);
+	if (!resp.status) {
+		alert("Something went wrong!");
+		return;
+	}
+	resp.data.forEach(tanzaku => {
+		$(`#t${tanzaku.id}`).addClass("selected");
+	});
+});
+
+$(document).on("input", "#filter", function (e) {
+	let filter = $(this).val().toLowerCase();
+	if (filter === "") {
+		$(".tanzaku").css("display", "block");
+		return;
+	}
+	sappyou.forEach((tanzaku) => {
+		if (tanzaku.name.toLowerCase().includes(filter)) {
+			$(`#t${tanzaku.id}`).css("display", "block");
+		} else {
+			$(`#t${tanzaku.id}`).css("display", "none");
+		}
+	});
+});
+
+$(document).on("click", "#btn-confirm", function (e) {
+	e.preventDefault();
+	let resp = tdb_query("$TFM", 24, '' + current_sasa.id);
+	if (!resp.status) {
+		alert("Something went wrong!");
+		return;
+	}
+	$(".menu-wrapper").css("display", "none");
+	resp.data.forEach(tanzaku => {
+		let current = $(`#t${tanzaku.id}`)
+		if (current.hasClass("selected")) {
+			current.removeClass("selected");
+		} else {
+			if (!tdb_query("$TFM", 9, '' + current_sasa.id + ' ' + tanzaku.id).status) {
+				console.log("ERROR: failed to remove kazari: " + current_sasa.id + '-' + tanzaku.id);
+			}
+		}
+	});
+	$(".tanzaku.selected").each(function (index, element) {
+		if (!tdb_query("$TFM", 10, '' + current_sasa.id + ' ' + $(element).attr("id").slice(1))) {
+			console.log("ERROR: failed to add kazari: " + current_sasa.id + '-' + tanzaku.id);
+		}
+	});
+	sappyou.forEach(tanzaku => {
+		$(`#t${tanzaku.id}`).removeClass("selected");
+	});
+})
--- web/public/js/tfm.js
+++ web/public/js/tfm.js
@@ -2,8 +2,8 @@ var sasahyou = null, sappyou = null, shoppyou = null;
 var current_sasa = null, current_tanzaku = null;
 var sasa_modified = false, tanzaku_modified = false;
 
-function sasa_load(id) {
-	resp = tdb_query("$TFM", 16, id < 0 ? "" : `${id}`);
+function sasa_load() {
+	resp = tdb_query("$TFM", 16, "");
 	if (resp == null) {
 		$(location).attr("href", "/auth");
 		throw new Error("Unauthorized");
@@ -12,16 +12,11 @@ function sasa_load(id) {
 		alert("Something went wrong");
 		return;
 	}
-	if (id < 0) {
-		sasahyou = resp.data;
-		sasahyou.forEach((sasa) => {
-			$(".contents-wrapper").append(`<div class="sasa" id="s${sasa.id}" title="${sasa.path.split('/').slice(-1)}" style="background-image: url(${"/thumbs/" + sasa.path})"><div class="overlay"></div></div>`);
-		});
-	}
+	sasahyou = resp.data;
 }
 
-function tanzaku_load(id) {
-	resp = tdb_query("$TFM", 32, id < 0 ? "" : `${id}`);
+function tanzaku_load() {
+	resp = tdb_query("$TFM", 32, "");
 	if (resp == null) {
 		$(location).attr("href", "/auth");
 		throw new Error("Unauthorized");
@@ -30,12 +25,7 @@ function tanzaku_load(id) {
 		alert("Something went wrong");
 		return;
 	}
-	if (id < 0) {
-		sappyou = resp.data;
-		sappyou.forEach((tanzaku) => {
-			$("#tanzaku-list").append(`<div class="tanzaku" id="t${tanzaku.id}">${tanzaku.name}</div>`);
-		});
-	}
+	sappyou = resp.data;
 }
 
 function kazari_load() {
@@ -51,23 +41,16 @@ function kazari_load() {
 	shoppyou = resp.data;
 }
 
-$(window).on("load", function () {
-	sasa_load(-1);
-	tanzaku_load(-1);
-	kazari_load();
-});
-
 $(document).keyup(function (e) {
 	if (e.key === "Escape") {
 		$(".selected").removeClass("selected");
 	}
 });
 
-$(document).on("click", ".sasa", function (e) {
+$(document).on("click", ".item", function (e) {
 	let wasSelected = $(this).hasClass("selected");
 	if (!e.ctrlKey) {
-		$(".selected").removeClass("selected");
-		wasSelected = false;
+		$(".item.selected").removeClass("selected");
 	}
 	if (wasSelected) {
 		$(this).removeClass("selected");
@@ -76,39 +59,7 @@ $(document).on("click", ".sasa", function (e) {
 	}
 });
 
-$(document).on("dblclick", ".sasa", function (e) {
-	let id = parseInt($(this).attr("id").slice(1));
-	sasahyou.every(sasa => {
-		if (sasa.id === id) {
-			current_sasa = sasa;
-			return false;
-		}
-		return true;
-	});
-	$(".sasa.selected").removeClass("selected");
-	$(".menu-wrapper").css("display", "flex");
-	$("#sasa-name").val(decodeURI(current_sasa.path));
-	$("#btn-full").attr("href", "/files/" + current_sasa.path);
-	let resp = tdb_query("$TFM", 24, '' + id);
-	if (!resp.status) {
-		alert("Something went wrong!");
-		return;
-	}
-	resp.data.forEach(tanzaku => {
-		$(`#t${tanzaku.id}`).addClass("selected");
-	});
-});
-
-$(document).on("click", "#btn-close", function (e) {
-	e.preventDefault();
-	$("#sasa-menu").css("display", "none");
-	sappyou.forEach(tanzaku => {
-		$(`#t${tanzaku.id}`).removeClass("selected");
-	});
-});
-
-$(document).on("click", ".tanzaku", function (e) {
-	sasa_modified = true;
+$(document).on("click", ".list-item", function (e) {
 	if ($(this).hasClass("selected")) {
 		$(this).removeClass("selected");
 	} else {
@@ -116,45 +67,8 @@ $(document).on("click", ".tanzaku", function (e) {
 	}
 });
 
-$(document).on("input", "#tanzaku-filter", function (e) {
-	let filter = $(this).val().toLowerCase();
-	if (filter === "") {
-		$(".tanzaku").css("display", "block");
-		return;
-	}
-	sappyou.forEach((tanzaku) => {
-		if (tanzaku.name.toLowerCase().includes(filter)) {
-			$(`#t${tanzaku.id}`).css("display", "block");
-		} else {
-			$(`#t${tanzaku.id}`).css("display", "none");
-		}
-	});
-});
-
-$(document).on("click", "#btn-sasa-confirm", function (e) {
+$(document).on("click", "#btn-close", function (e) {
 	e.preventDefault();
-	let resp = tdb_query("$TFM", 24, '' + current_sasa.id);
-	if (!resp.status) {
-		alert("Something went wrong!");
-		return;
-	}
-	$("#sasa-menu").css("display", "none");
-	resp.data.forEach(tanzaku => {
-		let current = $(`#t${tanzaku.id}`)
-		if (current.hasClass("selected")) {
-			current.removeClass("selected");
-		} else {
-			if (!tdb_query("$TFM", 0b1001, '' + current_sasa.id + ' ' + tanzaku.id).status) {
-				console.log("ERROR: failed to remove kazari: " + current_sasa.id + '-' + tanzaku.id);
-			}
-		}
-	});
-	$(".tanzaku.selected").each(function (index, element) {
-		if (!tdb_query("$TFM", 0b1010, '' + current_sasa.id + ' ' + $(element).attr("id").slice(1))) {
-			console.log("ERROR: failed to add kazari: " + current_sasa.id + '-' + tanzaku.id);
-		}
-	});
-	sappyou.forEach(tanzaku => {
-		$(`#t${tanzaku.id}`).removeClass("selected");
-	});
-})
+	$(".menu-wrapper").css("display", "none");
+	$(".list-item").removeClass("selected");
+});
